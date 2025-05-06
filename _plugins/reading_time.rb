6d766a36edc5a6c86dcdfef42b71212450724e88
Diego Navarro <12802120+mrchucu1@users.noreply.github.com>
2025-05-06 14:48:07 -0600

    Adding some style

---
+++ _plugins/reading_time.rb
@@ -0,0 +1,10 @@
+module ReadingTimeFilter
+  def reading_time(input)
+    words_per_minute = 180
+    words = input.split.size
+    minutes = (words / words_per_minute).floor
+    "#{minutes} min"
+  end
+end
+
+Liquid::Template.register_filter(ReadingTimeFilter)
